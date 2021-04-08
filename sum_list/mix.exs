53fe59f5e282ab27e4f0bfe5b1bb40eea6fba0be
João Eliandro Germano <eliandrogermano@gmail.com>
2021-04-08 02:17:28 -0300

    Added color support for tests

--- sum_list/mix.exs
+++ sum_list/mix.exs
@@ -7,6 +7,7 @@ defmodule SumList.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
+      aliases: [test: "test --color"],
       deps: deps()
     ]
   end
