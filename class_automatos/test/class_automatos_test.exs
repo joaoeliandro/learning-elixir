17353b6e4dc7963041cc516971fb421660c32cac
João Eliandro Germano <eliandrogermano@gmail.com>
2021-04-09 20:28:40 -0300

    Added a new test

--- class_automatos/test/class_automatos_test.exs
+++ class_automatos/test/class_automatos_test.exs
@@ -3,9 +3,10 @@ defmodule ClassAutomatosTest do
   doctest ClassAutomatos
 
   test "should be able to return matched" do
+    assert ClassAutomatos.afd(",14") == {:ok, "matched values"}
     assert ClassAutomatos.afd("3,14") == {:ok, "matched values"}
-    assert ClassAutomatos.afd("32,1433") == {:ok, "matched values"}
     assert ClassAutomatos.afd("331.0") == {:ok, "matched values"}
+    assert ClassAutomatos.afd("32,1433") == {:ok, "matched values"}
   end
 
   test "should not be able to return matched without symbol" do
