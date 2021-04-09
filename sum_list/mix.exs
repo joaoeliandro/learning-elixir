79330f83bab3a58866686bc33885360b1af5c5db
João Eliandro Germano <eliandrogermano@gmail.com>
2021-04-09 03:08:59 -0300

    Installed covarage deps

--- sum_list/mix.exs
+++ sum_list/mix.exs
@@ -7,7 +7,8 @@ defmodule SumList.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      aliases: [test: "test --color"],
+      aliases: [test: "test --color --cover"],
+      test_coverage: [tool: LcovEx, output: "cover"],
       deps: deps()
     ]
   end
@@ -22,6 +23,7 @@ defmodule SumList.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:lcov_ex, "~> 0.1", only: :test, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
