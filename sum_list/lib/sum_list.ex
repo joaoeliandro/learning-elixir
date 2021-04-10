2f7a504c23814c6ba0ffddf0610a90d26d82ebdc
João Eliandro Germano <eliandrogermano@gmail.com>
2021-04-10 02:38:06 -0300

    Added Enum module to test sum list

--- sum_list/lib/sum_list.ex
+++ sum_list/lib/sum_list.ex
@@ -1,6 +1,8 @@
 defmodule SumList do
   def call(list), do: sum(list, 0)
 
+  def call_enum(list), do: Enum.sum(list)
+
   defp sum([], accumulator), do: accumulator
 
   defp sum([head | tail], accumulator) do
